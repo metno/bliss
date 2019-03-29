ad6951b30e7c9d8f29be6d9c1df756964298cee6
Cristian Lussana <cristianl@met.no>
2019-03-29 19:06:47 +0000

    developments

--- bliss.r
+++ bliss.r
@@ -874,7 +874,9 @@ oi_var_gridpoint_by_gridpoint<-function(i,
                                         xa_errvar_min=0.001) {
 # return(c(xa,xa_errvar,o_errvar,xidi,idiv,av))
 # NOTE: av is the leave-one-out CV. However, its errvar is not returned.
-#       todo: figure out how to compute the leave-one-out errvar.
+#       todo: figure out how to compute the leave-ione-out errvar.
+# global variables: xgrid_spint, ygrid_spint, yb_spint, xb_spint
+#                   VecX, VecY, dh, dh2,
   deltax<-abs(xgrid_spint[i]-VecX)
   deltay<-abs(ygrid_spint[i]-VecY)
   if (y_elab) {
@@ -1049,6 +1051,8 @@ p <- add_argument(p, "--prId.exclude",
                   type="numeric",
                   default=NULL,
                   nargs=Inf)
+# OI_firstguess
+
 # additional OI parameters two-step temperature
 p <- add_argument(p, "--nmaxo",
                   help="number of closest observations to be used in the OI to adjust background values at a grid point",
@@ -1745,6 +1749,27 @@ p <- add_argument(p, "--cores",
                   default=NA)
 p <- add_argument(p, "--min_plaus_val",
                   help="minimum plausible value (e.g., 0 for prec)",
+                  type="numeric",
+                  default=NA)
+# observation post-processing
+p <- add_argument(p, "--obspp.agg",
+                  help="observation post-processing, aggregation",
+                  flag=T)
+p <- add_argument(p, "--obspp.agg_prId",
+                  help="observation post-processing, list of provider Ids (NA stands for all)",
+                  type="numeric",
+                  nargs=Inf,
+                  default=NA)
+p <- add_argument(p, "--obspp.agg_fact",
+                  help="observation post-processing, list of aggregation factors (with respect to the master grid)",
+                  type="numeric",
+                  nargs=Inf,
+                  default=NA)
+p <- add_argument(p, "--obspp.dqcpuddle",
+                  help="observation post-processing, data quality control puddle check",
+                  flag=T)
+p <- add_argument(p, "--obspp.dqcpuddle_fact",
+                  help="observation post-processing, dqc puddle aggregation factor (with respect to the master grid)",
                   type="numeric",
                   default=NA)
 #------------------------------------------------------------------------------
@@ -2533,13 +2558,237 @@ if (!is.na(argv$rrinf)) {
   nwet<-length(ixwet)
   ndry<-length(ixdry)
 }
-# Easy-peasy
-#if (nwet==0) {
-#  if (argv$verbose) print("no rain over the whole domain")
-#  writeIfNoPrec()
-#  if (argv$verbose) print("Success exit")
-#  quit(status=0)
-#}
+# Aggregate observations
+if (argv$obspp.agg) {
+  for (i in 1:length(argv$obspp.agg_prId)) {
+    if (is.na(argv$obspp.agg_prId[i])) {
+      ix<-1:n0
+    } else {
+      ix<-which(prId==argv$obspp.agg_prId[i])
+    }
+    if (length(ix)==0) next
+    if (argv$obspp.agg_fact[i]>1) {
+      r<-aggregate(rmaster, fact=argv$obspp.agg_fact[i], expand=T, na.rm=T)
+    } else {
+      r<-rmaster
+    }
+    r1<-rasterize(x=cbind(VecX[ix],VecY[ix]),
+                  y=r,
+                  field=yo[ix],
+                  fun=mean,
+                  na.rm=T)
+    if (length(ix)!=n0) {
+      VecX<-VecX[-ix]
+      VecY<-VecY[-ix]
+      VecXorig<-VecXorig[-ix]
+      VecYorig<-VecYorig[-ix]
+      VecLat<-VecLat[-ix]
+      VecLon<-VecLon[-ix]
+      VecZ<-VecZ[-ix]
+      VecS<-VecS[-ix]
+      yo<-yo[-ix]
+      if (exists("rrf"))  yrf<-yrf[-ix]
+      if (exists("rlaf")) VecLaf<-VecLaf[-ix]
+      prId<-prId[-ix]
+    }
+    ix1<-which( !is.na(getValues(r1)) )
+    if (exists("rrf")) ix1<-ix1[which(!is.na( extract(rrf,
+                                               cbind(xyFromCell(r1,ix1)[,1],
+                                                     xyFromCell(r1,ix1)[,2]),na.rm=T)))]
+    if (length(ix1)==0) next
+    v1<-getValues(r1)[ix1]
+    x1<-xyFromCell(r1,ix1)[,1]
+    y1<-xyFromCell(r1,ix1)[,2]
+    if (argv$iff_obs.proj4!=argv$grid_master.proj4) {
+      xymaster<-spTransform(SpatialPoints(cbind(x1,y1),
+                                          proj4string=CRS(argv$grid_master.proj4)) ,
+                            CRS(argv$iff_obs.proj4))
+      x1_orig<-attr(xymaster,"coords")[,1]
+      y1_orig<-attr(xymaster,"coords")[,2]
+      rm(xymaster)
+    } else {
+      x1_orig<-x1
+      y1_orig<-y1
+    }
+    if (argv$iff_obs.proj4!=proj4.llwgs84) {
+      xyll<-spTransform(SpatialPoints(cbind(x1,y1),
+                                      proj4string=CRS(argv$grid_master.proj4)) ,
+                        CRS(proj4.llwgs84))
+      x1_ll<-attr(xyll,"coords")[,1]
+      y1_ll<-attr(xyll,"coords")[,2]
+      rm(xyll)
+    } else {
+      x1_ll<-x1
+      y1_ll<-y1
+    }
+    VecX<-c(VecX,x1)
+    VecY<-c(VecY,y1)
+    VecXorig<-c(VecXorig,x1_orig)
+    VecYorig<-c(VecYorig,y1_orig)
+    VecLat<-c(VecLat,x1_ll)
+    VecLon<-c(VecLon,y1_ll)
+    VecZ<-c(VecZ,rep(NA,length(x1)))
+    VecS<-c(VecS,rep(NA,length(x1)))
+    yo<-c(yo,v1)
+    if (exists("rrf"))  yrf<-c(yrf,extract(rrf,cbind(x1,y1),na.rm=T))
+    if (exists("rlaf")) VecLaf<-c(VecLaf,extract(rlaf,cbind(x1,y1),na.rm=T))
+    prId<-c(prId,rep(argv$obspp.agg_prId[i],length(x1)))
+  } # end loop over argv$obspp.agg_prId
+  n0<-length(yo)
+  ydqc.flag<-rep(0,length=n0)
+  if (!is.na(argv$rrinf)) {
+    ixwet<-which(yo>=argv$rrinf)
+    ixdry<-which(yo< argv$rrinf)
+    nwet<-length(ixwet)
+    ndry<-length(ixdry)
+  }
+  rm(x1,y1,x1_ll,y1_ll,x1_orig,y1_orig,r,r1,ix)
+} # end observation aggregation
+#
+# ad-hoc data quality control
+if (argv$obspp.dqcpuddle) {
+  if (argv$mode=="OI_firstguess") {
+    # set eps2 (station-dependent and value-dependent)
+    eps2<-vector(mode="numeric",length=n0)
+    ixdef<-which(is.na(argv$oifg.eps2_prId))
+    for (r in 1:(length(argv$oifg.eps2_r)-1)) {
+      ixr<-which(yo>=argv$oifg.eps2_r[r] & 
+                 yo<argv$oifg.eps2_r[(r+1)])
+      eps2[ixr]<-argv$oifg.eps2[r,ixdef]
+      for (i in 1:length(argv$oifg.eps2_prId)) {
+        if (is.na(argv$oifg.eps2_prId[i])) next
+        ixr<-which(yo>=argv$oifg.eps2_r[r] & 
+                   yo<argv$oifg.eps2_r[(r+1)] & 
+                 prId==argv$oifg.eps2_prId[i]) 
+        eps2[ixr]<-argv$oifg.eps2[r,i]
+      }
+    }
+    # dh (m), one for all gridpoints
+    dh<-argv$oifg.Dh*1000
+    dh2<-dh*dh
+    # background at station locations
+    yb<-extract(rfg, cbind(VecX,VecY), method="bilinear")
+    # create a coarser grid
+    if (argv$obspp.dqcpuddle_fact>1) {
+      r<-aggregate(rfg, fact=argv$obspp.dqcpuddle_fact, expand=T, na.rm=T)
+    } else {
+      r<-rmaster
+    }
+    xix<-which(!is.na(getValues(r)))
+    # check: at least 10 events are needed on the grid
+    if (length(xix)>10) {
+      xgrid_spint<-xyFromCell(r,xix)[,1]
+      ygrid_spint<-xyFromCell(r,xix)[,2]
+      ngrid_spint<-length(ygrid_spint)
+      # -- elaboration on the grid, x_elab --
+      t00<-Sys.time()
+      if (argv$transf=="Box-Cox") {
+        xb_spint<-getValues(r)[xix]
+        yo_spint<-yo
+        yb_spint<-yb
+        xb_spint[which(xb_spint<0)]<-0
+        yb_spint[which(yb_spint<0)]<-0
+        yo_spint[which(yo_spint<0)]<-0
+        yo_spint<-boxcox(yo_spint,argv$transf.boxcox_lambda)
+        yb_spint<-boxcox(yb_spint,argv$transf.boxcox_lambda)
+        xb_spint<-boxcox(xb_spint,argv$transf.boxcox_lambda)
+      } else {
+        xb_spint<-getValues(r)[xix]
+        yo_spint<-yo
+        yb_spint<-yb
+      }
+      # multicores
+      # xa=arr[,1], xa_errvar=arr[,2], o_errvar=arr[,3], xidi=arr[,4], idiv=arr[,5]
+      if (!is.na(argv$cores)) {
+        arr<-t(mcmapply(oi_var_gridpoint_by_gridpoint,
+                        1:ngrid_spint,
+                        mc.cores=argv$cores,
+                        SIMPLIFY=T,
+                        pmax=argv$pmax,
+                        corr=argv$corrfun))
+      # no-multicores
+      } else {
+        arr<-t(mapply(oi_var_gridpoint_by_gridpoint,
+                      1:ngrid_spint,
+                      SIMPLIFY=T,
+                      pmax=argv$pmax,
+                      corr=argv$corrfun))
+      }
+      if (argv$verbose) {
+        t11<-Sys.time()
+        print(paste("obspp_dqcpuddle elab, time=",
+                    round(t11-t00,1),attr(t11-t00,"unit")))
+      }
+      # loop over dqc thresholds
+      for (i in 1:length(argv$obspp.dqcpuddle_thres)) {
+        minarea<-ceiling(1000000*argv$obspp.dqcpuddle_minarea[i]/(xres(r)*yres(r)))
+        r[]<-NA
+        thr<-argv$obspp.dqcpuddle_thres[i]
+        if (argv$transf=="Box-Cox") 
+          thr<-boxcox(argv$obspp.dqcpuddle_thres[i],argv$transf.boxcox_lambda)
+        # T= event occurred; F=event not occurred
+        if (argv$obspp.dqcpuddle_cond[i]=="lt") {
+          xcond<-arr[,1]<thr
+          ycond<-yo<argv$obspp.dqcpuddle_thres[i]
+        } else if (argv$obspp.dqcpuddle_cond[i]=="le") {
+          xcond<-arr[,1]<=thr
+          ycond<-yo<=argv$obspp.dqcpuddle_thres[i]
+        } else if (argv$obspp.dqcpuddle_cond[i]=="gt") {
+          xcond<-arr[,1]>thr 
+          ycond<-yo>argv$obspp.dqcpuddle_thres[i]
+        } else if (argv$obspp.dqcpuddle_cond[i]=="ge") {
+          xcond<-arr[,1]>=thr
+          ycond<-yo>=argv$obspp.dqcpuddle_thres[i]
+        }
+        if (!any(xcond)) next
+        r[xix[which(xcond)]]<-1
+        yix<-which(ycond)
+        if (length(yix)==0) next
+        rc<-clump(r)
+        dc<-getValues(rc)
+        f<-freq(rc)
+        ytmp<-extract(rc,cbind(VecX[yix],VecY[yix]),na.rm=T,small=T)
+        if (!any(!is.na(ytmp))) next
+        nobs_in_f<-mapply(function(x){
+                           if (is.na(f[x,1])) {
+                             length(which(is.na(ytmp)))
+                           } else {
+                             length(which(!is.na(ytmp) & ytmp==f[x,1]))
+                           }},
+                           1:length(f[,1]),
+                           SIMPLIFY=T)
+        # observation is suspect if:
+        # a. the region where the event occurred is too small
+        # b. the are too few observation in the region where the event occurred
+        ixsus<-yix[which(
+            is.na(ytmp) |
+            f[match(ytmp,f),2]<minarea | # a.
+            nobs_in_f[match(ytmp,f)]<argv$obspp.dqcpuddle_nminobs[i])] # b.
+        if (length(ixsus)==0) next
+        VecX<-VecX[-ixsus]
+        VecY<-VecY[-ixsus]
+        VecXorig<-VecXorig[-ixsus]
+        VecYorig<-VecYorig[-ixsus]
+        VecLat<-VecLat[-ixsus]
+        VecLon<-VecLon[-ixsus]
+        VecZ<-VecZ[-ixsus]
+        VecS<-VecS[-ixsus]
+        yo<-yo[-ixsus]
+        if (exists("rrf"))  yrf<-yrf[-ixsus]
+        if (exists("rlaf")) VecLaf<-VecLaf[-ixsus]
+        prId<-prId[-ixsus]
+      } # loop over dqc thresholds
+      n0<-length(yo)
+      ydqc.flag<-rep(0,length=n0)
+      if (!is.na(argv$rrinf)) {
+        ixwet<-which(yo>=argv$rrinf)
+        ixdry<-which(yo< argv$rrinf)
+        nwet<-length(ixwet)
+        ndry<-length(ixdry)
+      }
+    } # endif, check: at least 10 events are needed on the grid 
+  } #end if (argv$mode=="OI_firstguess") {
+}
 # observation error variance correction factor
 ovarc<-rep(1,n0)
 if (any(!is.na(argv$ovarc.prId))) {
@@ -2699,14 +2948,34 @@ if (argv$mode=="OI_firstguess") {
                     corr=argv$corrfun))
     }
     xidi<-arr[,4]
+    argv$oifg.xta_errvar_smooth<-50000
     if (argv$transf=="Box-Cox") {
       xta<-arr[,1]
+      # smooth out xta_errvar 
+      # first, fill NAs on
       r<-rmaster;r[]<-NA
       r[aix]<-arr[,2]
       r1<-r
-      for (ii in 1:10) r1<-focal(r1,w=matrix(1,25,25),fun=mean,na.rm=T,NAonly=T)
-      r1<-focal(r1,w=matrix(1,25,25),fun=mean,na.rm=T)
-      xta_errvar<-getValues(r1)[aix]; rm(r,r1)
+      focalx<-ceiling(argv$oifg.xta_errvar_smooth/xres(rmaster))
+      focaly<-ceiling(argv$oifg.xta_errvar_smooth/yres(rmaster))
+      if ((focalx%%2)==0) focalx<-focalx+1
+      if ((focaly%%2)==0) focaly<-focaly+1
+      for (ii in 1:10) {
+        r1<-focal(r1,w=matrix(1,focalx,focaly),fun=mean,na.rm=T,NAonly=T)
+        if (!any(is.na(getValues(r1)[aix]))) break
+      }
+      # second, smooth and make a continuous transition with non-prec regions
+      for (ii in 1:3) {
+        if (ii==2) {focalx<-5;focaly<-5}
+        if (ii==3) {focalx<-3;focaly<-3}
+        aux<-getValues(r1)[aix]
+        aux[is.na(aux)|xta<boxcox(argv$rrinf,argv$transf.boxcox_lambda)]<-0
+        r1[aix]<-aux
+        r1[is.na(r1)]<-0
+        r1<-focal(r1,w=matrix(1,focalx,focaly),fun=mean,na.rm=T)
+      }
+      xta_errvar<-getValues(r1)[aix]; rm(r,r1,focalx,focaly,aux)
+      #
       xtb<-xb_spint
       xbctmp<-cbind(xta,sqrt(abs(xta_errvar)))
       if (!is.na(argv$cores)) {
