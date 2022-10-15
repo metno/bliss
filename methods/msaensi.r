114ef70651370a864f79bbf0b4b9d84015933ce4
cristianlussana <cristian.lussana@met.no>
2022-10-15 18:26:57 +0200

    working on the msaensi algorithm

--- methods/msaensi.r
+++ methods/msaensi.r
@@ -1,12 +1,5 @@
 #+
 msaensi <- function( argv, y_env, fg_env, env) {
-#                     supob_nobs=50, supob_radius=1500, supob_q=0.99,
-#                                max_it=100, opttol=0.02,
-#                                En2_adj_fun="Gaussian",
-#                                En2_adj_min=0,
-#                                rescale_min_obs=100, rescale_min_cells=100,
-#                                plot=F, dir_plot=NA) {
-#
 #------------------------------------------------------------------------------
   library(waveslim)
   library(smoothie)
@@ -120,38 +113,83 @@ msaensi <- function( argv, y_env, fg_env, env) {
 
     mrbkg$data[[j]] <- list()
     mrbkg$data[[j]]$E <- array( data=NA, dim=c( mrtree$m_dim[[j]], env$k_dim))
-    mrbkg$data[[j]]$Eor <- array( data=NA, dim=c( mrtree$m_dim[[1]], env$k_dim))
     mrbkg$data[[j]]$HE <- array( data=NA, dim=c( mrobs$d_dim[[j]], env$k_dim))
+    mrbkg$data[[j]]$X  <- array( data=NA, dim=c( mrtree$m_dim[[j]], env$k_dim))
+    mrbkg$data[[j]]$Z   <- array( data=NA, dim=c( mrtree$m_dim[[j]], env$k_dim))
+    mrbkg$data[[j]]$Y   <- array( data=NA, dim=c( mrobs$d_dim[[j]], env$k_dim))
+    if (j == jstop) { 
+      mrbkg$data[[1]] <- list()
+      mrbkg$data[[1]]$Eor <- array( data=NA, dim=c( mrtree$m_dim[[1]], env$k_dim))
+    }
 
-    # prepare the background 
+    # loop over ensemble members - prepare the background 
     for (e in 1:env$k_dim) {
+      # first iteration, background is from the original ensemble
       if ( j == jstop ) {
         r <- resample( subset( fg_env$fg[[fg_env$ixf[fg_env$ixs[e]]]]$r_main, subset=fg_env$ixe[fg_env$ixs[e]]), rdyad, method="bilinear")
         if (!is.na(y_env$rain)) r[r<y_env$rain] <- 0
-        mrbkg$data[[j]]$Eor[,e] <- getValues(r) 
+        mrbkg$data[[1]]$Eor[,e] <- getValues(r) 
+      # 2nd,3rd,... iterations, background is from the previous iteration
       } else {
         aux <- idwt.2d( dwt[[e]])
-#        if (!is.na(y_env$rain)) aux[aux<y_env$rain] <- 0
+        # remove trace of precipitation
+        if (!is.na(y_env$rain)) aux[aux<y_env$rain] <- 0
         if (any(is.na(aux))) aux[is.na(aux)] <- 0
-#        aux <- mrbkg$data[[j]]$Eor[,e] + mrobs$idi[[1]] * gauss2dsmooth(x=aux,lambda=2,nx=sqrt(mrtree$m_dim[1]),ny=sqrt(mrtree$m_dim[1])) *
-        r <- mrtree$raster[[1]]$r
+        r   <- mrtree$raster[[1]]$r 
         r[] <- array(data=as.matrix(aux),dim=c(sqrt(mrtree$m_dim[1]),sqrt(mrtree$m_dim[1])))
-        s <- mrtree$raster[[1]]$r
-        s[] <- mrbkg$data[[jstop]]$Eor[,e]
-        t <- r - s
+        x_bak <- getValues(r)
+        x_aux <- getValues(r)
+        x_aux[which(!is.na(x_aux) & x_aux==0)] <- NA
+        r[]<-x_aux
+        rclump<-clump(r)
+#        oclump<-extract(rclump,cbind(VecX[ixwet],VecY[ixwet]))
+        fr<-freq(rclump)
+        # remove clumps of YESprec cells less than (4x4)km^2 or not including wet obs
+#        ix<-which(!is.na(fr[,1]) & !is.na(fr[,2]) & ( (fr[,2]<=100) | !(fr[,1] %in% oclump)) )
+        ix<-which(!is.na(fr[,1]) & !is.na(fr[,2]) &  fr[,2]<=100)
+        x_bak[which(getValues(rclump) %in% fr[ix,1])] <- 0
+        r[]<-x_bak
+        rm(x_aux,rclump,fr,ix,x_bak)
+        # smooth the background, where it is different from the original background
+#        r   <- mrtree$raster[[1]]$r 
+#        r[] <- array(data=as.matrix(aux),dim=c(sqrt(mrtree$m_dim[1]),sqrt(mrtree$m_dim[1])))
+        s   <- mrtree$raster[[1]]$r
+        s[] <- mrbkg$data[[1]]$Eor[,e]
+        t   <- r - s
 #        t[abs(getValues(t))<=0.5]<-0
-        aux <- gauss2dsmooth(x=as.matrix(t),lambda=2,nx=sqrt(mrtree$m_dim[1]),ny=sqrt(mrtree$m_dim[1]))
+#        lambda <- mrtree$mean_res[[j]]/mrtree$mean_res[[1]]
+        lambda <- 2
+        aux <- gauss2dsmooth( x=as.matrix(t), lambda=lambda, 
+                              nx=sqrt(mrtree$m_dim[1]), ny=sqrt(mrtree$m_dim[1]))
         t[] <- aux
-        r <- s + t 
+        r   <- s + t
+        if (!is.na(y_env$rain)) r[r<y_env$rain] <- 0
+        rm( s, t)
       }
+      #discrete wavelet transformation
       dwt[[e]] <- dwt.2d( as.matrix(r), wf=argv$msaensi_wf, J=jw, boundary=argv$msaensi_boundary)
-      # father wavelet as the approximated field at the j-th level
-      r <- mrtree$raster[[j]]$r
+      # father wavelet as the reconstructed field at the j-th spatial level
+      r   <- mrtree$raster[[j]]$r
       r[] <- dwt[[e]][[3*jw+1]] / 2**jw
-      mrbkg$data[[j]]$E[,e] <- getValues(r)
+      # store the background in the multiresolution tree
+      mrbkg$data[[j]]$E[,e]  <- getValues(r)
       mrbkg$data[[j]]$HE[,e] <- extract( r, cbind( mrobs$x[[j]], mrobs$y[[j]]))
+    } # end loop over ensemble members
+    mrbkg$data[[j]]$x <- rowMeans(mrbkg$data[[j]]$E)
+    # background ensemble anomalies 
+    mrbkg$data[[j]]$Esd <- apply( mrbkg$data[[j]]$E, FUN=function(x){sd(x)}, MAR=1)
+    for (e in 1:env$k_dim) { 
+      # covariances
+      mrbkg$data[[j]]$X[,e] <- 1/sqrt(env$k_dim-1) * (mrbkg$data[[j]]$E[,e] - mrbkg$data[[j]]$x)
+      # correlations
+      mrbkg$data[[j]]$Z[,e] <- 1/sqrt(env$k_dim-1) * (mrbkg$data[[j]]$E[,e] - mrbkg$data[[j]]$x) / mrbkg$data[[j]]$Esd
+      mrbkg$data[[j]]$Z[,e][!is.finite(mrbkg$data[[j]]$Z[,e])] <- 1/sqrt(env$k_dim) 
+      r   <- mrtree$raster[[j]]$r
+      r[] <- mrbkg$data[[j]]$Z[,e]
+      mrbkg$data[[j]]$Y[,e] <- extract( r, cbind( mrobs$x[[j]], mrobs$y[[j]]), method="simple")
     }
-    # Spatial analysis
+
+    # Spatial analysis with multiresolution background and observations
     envtmp$x <- mrtree$x[[j]]
     envtmp$y <- mrtree$y[[j]]
     envtmp$m_dim <- mrtree$m_dim[[j]]
@@ -163,6 +201,8 @@ msaensi <- function( argv, y_env, fg_env, env) {
     envtmp$obs_val <- mrobs$val[[j]]
     envtmp$Eb <- mrbkg$data[[j]]$E
     envtmp$HE <- mrbkg$data[[j]]$HE
+    envtmp$Y <- mrbkg$data[[j]]$Y
+    envtmp$Z <- mrbkg$data[[j]]$Z
     envtmp$D <- envtmp$obs_val - envtmp$HE
     envtmp$eps2 <- rep( argv$msa_eps2, envtmp$m_dim) 
     envtmp$nn2 <- nn2( cbind(mrobs$x[[j]],mrobs$y[[j]]), 
@@ -170,22 +210,29 @@ msaensi <- function( argv, y_env, fg_env, env) {
                        k = min( c(argv$pmax,mrobs$d_dim[[j]])), 
                        searchtype = "radius", 
                        radius = (7*mrtree$mean_res[[j]]))
+#save(file="tmp.rdata",envtmp)
     # run EnKF/EnOI gridpoint by gridpoint
     if (!is.na(argv$cores)) {
-      res <- t( mcmapply( enoi_basic_gridpoint_by_gridpoint,
+      res <- t( mcmapply( enoi_Evensen2003_gridpoint_by_gridpoint,
                           1:envtmp$m_dim,
                           mc.cores=argv$cores,
                           SIMPLIFY=T,
                           MoreArgs = list( corr=argv$corrfun,
                                            dh=mrtree$mean_res[[j]],
+                                           dh_loc=(2*mrtree$mean_res[[j]]),
+                                           alpha=0.5,
+                                           k_dim_corr=envtmp$k_dim,
                                            idi=F)))
     # no-multicores
     } else {
-      res <- t( mapply( enoi_basic_gridpoint_by_gridpoint,
+      res <- t( mapply( enoi_Evensen2003_gridpoint_by_gridpoint,
                         1:envtmp$m_dim,
                         SIMPLIFY=T,
                         MoreArgs = list( corr=argv$corrfun, 
                                          dh=mrtree$mean_res[[j]],
+                                         dh_loc=(2*mrtree$mean_res[[j]]),
+                                         alpha=0.5,
+                                         k_dim_corr=envtmp$k_dim,
                                          idi=F)))
     }
     Ea <- res[,1:env$k_dim]
@@ -206,13 +253,12 @@ msaensi <- function( argv, y_env, fg_env, env) {
       # Align smaller scales
       of_u <- of$u
       of_v <- of$v
+      # do not align when idi is 0
+      # note: idi is calculated by mergeobs
       of_u[mrobs$idi[[j]]==0] <- 0
       of_v[mrobs$idi[[j]]==0] <- 0
-#of_u[]<-0
-#of_v[]<-0
       # Loop over scales
       for (jj in j:2) {
-#        print(jj)
         jjw <- jj-1
 #        of_u <- resample( of_u, mrtree$raster[[jj]]$r, method="bilinear")
 #        of_v <- resample( of_v, mrtree$raster[[jj]]$r, method="bilinear")
@@ -230,14 +276,14 @@ msaensi <- function( argv, y_env, fg_env, env) {
       }  # END - Loop over scales
       r <- mrtree$raster[[j]]$r  
       r[] <- Ea[,e] * 2**jw
-#r[] <- mrbkg$data[[j]]$E[,e] * 2**jw
       dwt[[e]][[3*jw+1]][] <- as.matrix(r)
 #      rm( rb_finer, of_u, of_v, rbmod_finer)
-#      Eaj <- idwt.2d( dwt[[e]])
-#      r <- mrtree$raster[[1]]$r
-#      r[] <- array(data=as.matrix(Eaj),dim=c(sqrt(mrtree$m_dim[1]),sqrt(mrtree$m_dim[1])))
+#e<-1;Eaj <- idwt.2d( dwt[[e]]);r <- mrtree$raster[[1]]$r;r[] <- array(data=as.matrix(Eaj),dim=c(sqrt(mrtree$m_dim[1]),sqrt(mrtree$m_dim[1])));r[r<0.1]<-0;image(r,breaks=c(-100,0,0.1,1,2,4,8),col=c("beige","gray",rev(rainbow(4))))
+#s<-r;s[]<-mrbkg$data[[1]]$Eor[,e]; image(s,breaks=c(-100,0,0.1,1,2,4,8),col=c("beige","gray",rev(rainbow(4))))
+#t<-r;t[]<-mrobs$idi[[1]]; image(t,breaks=c(-100,0,0.1,1,2,4,8),col=c("beige","gray",rev(rainbow(4))))
 #png(file="test.png",width=1200,height=1200)
 #image(r,breaks=c(-100,0,0.1,1,2,4,8),col=c("beige","gray",rev(rainbow(4)))) 
+
 #save(file="tmp.rdata",r,env,dwt,mrbkg,envtmp,j,jw,mrtree,mrobs,argv,y_env)
 #dev.off()
 #q()
---
+++ src/enoi_Evensen2003_gridpoint_by_gridpoint.r
@@ -0,0 +1,63 @@
+#+ 
+enoi_Evensen2003_gridpoint_by_gridpoint <- function( i,
+                                                     corr = "soar",
+                                                     dh = 10000,
+                                                     dh_loc = 10000,
+                                                     alpha = 0.5,
+                                                     k_dim_corr=10,
+                                                     idi = F) {
+# returned values: analysis, observation error var, analysis error var
+# note: the call ‘t(x) %*% y’ (‘crossprod’) or ‘x %*% t(y)’ (‘tcrossprod’)
+#------------------------------------------------------------------------------
+  xidi <- NA
+
+  if( i%%(round(envtmp$m_dim/10)) == 0) cat(".")
+
+  # select the observations to use
+  if ( (p <- length( aux <- which(envtmp$nn2$nn.idx[i,]!=0))) == 0) {
+
+    # no observations, analysis=background
+    Ea <- envtmp$Eb[i,]
+  } else {
+
+    # available observations
+    ixa  <- envtmp$nn2$nn.idx[i,aux]
+    di <- array( data=envtmp$D[ixa,], dim=c(p,envtmp$k_dim))
+    if (envtmp$k_dim>k_dim_corr) {
+      ixb <- order( colMeans( abs(di)))[1:k_dim_corr]
+    } else {
+      ixb <- 1:envtmp$k_dim
+      k_dim_corr <- envtmp$k_dim
+    }
+
+    # define vectors
+    dist <- envtmp$nn2$nn.dists[i,aux]
+    x <- envtmp$obs_x[ixa]
+    y <- envtmp$obs_y[ixa]
+#    di <- array( data=envtmp$D[ixa,], dim=c(p,envtmp$k_dim))
+    Zi <- array( data=envtmp$Z[i,ixb],   dim=c(1,k_dim_corr))
+    Yi <- array( data=envtmp$Y[ixa,ixb], dim=c(p,k_dim_corr))
+    eps2 <- envtmp$eps2[i]
+
+    # localization
+    loc1d <- corr1d( dist, dh_loc, corr)
+    loc2d <- corr2d( cbind(x,y), dh_loc, corr)
+
+    # static correlations
+    rloc <- corr1d( dist, dh, corr)
+    Cyy_s <- corr2d( cbind(x,y), dh, corr)
+
+    # combine static and dynamic correlations
+    Cxy <- alpha * rloc + (1-alpha) * loc1d * tcrossprod( Zi, Yi)
+    Cyy <- alpha * Cyy_s + (1-alpha) * loc2d * tcrossprod( Yi, Yi)
+
+    #
+    CyyCdd_inv <- chol2inv( chol( (Cyy+diag(x=eps2,p)) ))
+    CyyCdd_inv_di <- crossprod( CyyCdd_inv, di)       
+    Ea <- envtmp$Eb[i,] + crossprod( t(Cxy), CyyCdd_inv_di)
+
+    if (idi) xidi <- sum( Cxy * as.vector(rowSums(CyyCdd_inv)))
+  }
+  return( c( Ea, xidi))
+}
+
